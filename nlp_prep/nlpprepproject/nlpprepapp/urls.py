1bf12b180c84f8a5a77bb0e4e76dca9db06c224b
chandrakanth <chandrakanth.lns@nslhub.com>
2020-07-15 19:54:32 +0530

    removing my code

--- nlp_prep/nlpprepproject/nlpprepapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    
-    path('get_health', views.get_health, name='get_health'),
-    path('nlp_prep', views.nlp_prep, name='nlp_prep'),
-]
